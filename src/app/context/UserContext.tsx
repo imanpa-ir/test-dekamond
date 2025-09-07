bf97c46352ac215f19e1c4edb92b6081c913bffc
imanpa-ir <imanpa1100@gmail.com>
2025-09-08 00:21:57 +0330

    update Type user

--- src/app/context/UserContext.tsx
+++ src/app/context/UserContext.tsx
@@ -1,47 +1,21 @@
 'use client'
-
-import {createContext, useContext, useEffect, useState} from 'react'
-
-type User = any | null //  همه داده‌های API ذخیره میشه
+import {createContext, ReactNode, useContext, useState} from 'react'
+import {UserType} from '@/types/user'
 
 type UserContextType = {
-    user: User
-    setUser: (user: User) => void
-    logout: () => void
+    user: UserType | null
+    setUser: (user: UserType | null) => void
 }
 
 const UserContext = createContext<UserContextType | undefined>(undefined)
 
-export function UserProvider({children}: {children: React.ReactNode}) {
-    const [user, setUserState] = useState<User>(null)
+export const UserProvider = ({children}: {children: ReactNode}) => {
+    const [user, setUser] = useState<UserType | null>(null)
 
-    //  بعد از رفرش → localStorage رو بررسی کن
-    useEffect(() => {
-        const storedUser = localStorage.getItem('user')
-        if (storedUser) {
-            try {
-                setUserState(JSON.parse(storedUser))
-            } catch {
-                localStorage.removeItem('user')
-            }
-        }
-    }, [])
-
-    const setUser = (newUser: User) => {
-        setUserState(newUser)
-        if (newUser) {
-            localStorage.setItem('user', JSON.stringify(newUser))
-        } else {
-            localStorage.removeItem('user')
-        }
-    }
-
-    const logout = () => setUser(null)
-
-    return <UserContext.Provider value={{user, setUser, logout}}>{children}</UserContext.Provider>
+    return <UserContext.Provider value={{user, setUser}}>{children}</UserContext.Provider>
 }
 
-export function useUser() {
+export const useUser = () => {
     const context = useContext(UserContext)
     if (!context) throw new Error('useUser must be used within a UserProvider')
     return context
